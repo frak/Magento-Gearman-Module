68fba44030f7bf03e192621d8b501f4181986923
Michael Davey <mikey@ibuildings.com>
2010-11-26 12:10:45 +0000

    Modified tests to make the event dispatcher test more rigourous
    Well, it actually has some rigour now, put it that way

--- code/local/Ibuildings/Gearman/tests/ObserverTest.php
+++ code/local/Ibuildings/Gearman/tests/ObserverTest.php
@@ -4,13 +4,31 @@ class ObserverTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase
 {
     public function testDispatchTask()
     {
-        $e = array();
+        $ps = explode("\n", `ps ax | grep test_worker`);
+        $found = false;
+        foreach ($ps as $line) {
+            if (preg_match('/php test_worker\.php/', $line)) {
+                $found = true;
+                break;
+            }
+        }
+        $this->assertTrue($found);
+        $e          = array();
         $e['queue'] = 'test';
+        $id         = uniqid();
+        $data       = 'This is a string!';
         $e['task']  = array(
-            'id' => 1234,
-            'payload' => 'This is a string!',
+            'id' => $id,
+            'payload' => $data,
             'callback' => 'http://magento.development.local/index.php'
         );
         Mage::dispatchEvent('gearman_do_async_task', $e);
+        sleep(1);
+        $log = explode(PHP_EOL, file_get_contents('./testing.log'));
+        $res = preg_match(
+            '/' . $id . ' \- ' . $data . '/',
+            $log[count($log) - 2]
+        );
+        $this->assertEquals(1, $res);
     }
 }
--- code/local/Ibuildings/Gearman/tests/test_worker.php
+++ code/local/Ibuildings/Gearman/tests/test_worker.php
@@ -1,5 +1,7 @@
 <?php
 
+@unlink('./testing.log');
+
 $worker = new GearmanWorker();
 $worker->addServer('127.0.0.1', 4730);
 $worker->addFunction('test', 'test_fn');
@@ -7,7 +9,7 @@ $worker->addFunction('test', 'test_fn');
 echo "Waiting for work...\n";
 while ($worker->work()) {
     if ($worker->returnCode() !== GEARMAN_SUCCESS) {
-        echo "Ooops: " . $worker->returnCode() . "\n";
+        echo 'Ooops: ' . $worker->returnCode() . PHP_EOL;
         break;
     }
     echo "\tWork done!\n";
@@ -17,13 +19,18 @@ function test_fn($job)
 {
     $job->sendStatus(0, 2);
     $stuff = unserialize($job->workload());
+    file_put_contents(
+        './testing.log',
+        $stuff['id'] . ' - ' . $stuff['payload'] . PHP_EOL,
+        FILE_APPEND
+    );
     $stuff['payload'] = strrev($stuff['payload']);
     sleep(1);
     $job->sendStatus(1, 2);
     if (isset($stuff['callback'])) {
         $stuff['result'] = file_get_contents($stuff['callback']);
-        $job->sendStatus(2, 2);
-        sleep(1);
     }
+    $job->sendStatus(2, 2);
+    sleep(1);
     return serialize($stuff);
 }

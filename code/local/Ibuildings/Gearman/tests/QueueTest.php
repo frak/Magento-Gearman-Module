71771f2c57e4a73232eab52e9822424cad6413d3
Michael Davey <mikey@ibuildings.com>
2010-11-26 17:05:02 +0000

    Added blocking call method test

--- code/local/Ibuildings/Gearman/tests/QueueTest.php
+++ code/local/Ibuildings/Gearman/tests/QueueTest.php
@@ -65,4 +65,21 @@ class QueueTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase
             $this->assertTrue($ret === 'done');
         }
     }
+
+    public function testBlockingTaskReturnsResults()
+    {
+        $ret = $this->_queue->blockingCall($this->getTask());
+        if (!class_exists('GearmanClient')) {
+            /*
+            $this->markTestIncomplete(
+                'This test only applies to the Gearman extension'
+            );
+            */
+            $this->assertNull($ret);
+        }
+        else{
+            print_r($ret);
+            $this->assertTrue($ret['payload'] == '!gnirts a si sihT');
+        }
+    }
 }

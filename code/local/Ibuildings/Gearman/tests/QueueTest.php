1f167803acfa3bc19a40a9d810bb9d3dacc59d87
Michael Davey <mdavey@ibuildings.com>
2010-11-30 14:22:02 +0000

    Use getClient() to increase code coverage

--- code/local/Ibuildings/Gearman/tests/QueueTest.php
+++ code/local/Ibuildings/Gearman/tests/QueueTest.php
@@ -10,7 +10,37 @@ class QueueTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase
         $this->mageBootstrap();
         $this->_queue = Mage::getModel('Ibuildings_Gearman_Model_Queue');
         $opts = array(
-            'gearman' => array('server' => '127.0.0.1', 'port' => 4730)
+            'gearman' => array(
+                'server' => '127.0.0.1',
+                'port' => 4730
+            )
+        );
+        $this->_queue->setGearmanClient($opts);
+
+        $ps = explode(PHP_EOL, `ps ax | grep test_worker`);
+        $found = false;
+        foreach ($ps as $line) {
+            if (preg_match('/test_worker\.php/', $line)) {
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            $this->fail(
+                'You need to have started test_worker.php from the ' .
+                'tests directory'
+            );
+        }
+        $this->assertTrue($found);
+    }
+
+    public function getQueue($type = '')
+    {
+        $opts = array(
+            'gearman' => array(
+                'server' => '127.0.0.1',
+                'port' => 4730,
+                'type' => $type)
         );
         $this->_queue->setGearmanClient($opts);
     }
@@ -20,7 +50,7 @@ class QueueTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase
         $t = array();
         $t['queue']    = 'test';
         $t['task']     = array(
-            'id'       => 1234,
+            'id'       => uniqid(),
             'payload'  => 'This is a string!',
             'callback' => self::BASE_URL . '/index.php'
         );
@@ -29,51 +59,71 @@ class QueueTest extends Ibuildings_Mage_Test_PHPUnit_ControllerTestCase
     
     public function testSubmitJobReturnsId()
     {
+        $this->getQueue('ext');
+        $this->assertType(
+            'GearmanClient',
+            $this->_queue->getGearmanClient()
+        );
         $id = $this->_queue->dispatchTask($this->getTask());
-        // Kludge added for Net_Gearman case...
-        if (!is_null($id)) {
-            $this->assertTrue(preg_match('/[A-Z]+\:[A-z\-_0-9]+\:[0-9]+/', $id) > 0);
-        }
+        $this->assertTrue(
+            preg_match('/[A-Z]+\:[A-z\-_0-9]+\:[0-9]+/', $id) > 0
+        );
+        $this->getQueue('net');
+        $this->assertType(
+            'Net_Gearman_Client',
+            $this->_queue->getGearmanClient()
+        );
+        $id = $this->_queue->dispatchTask($this->getTask());
+        $this->assertNull($id);
     }
 
     public function testCheckTaskCompleteReturnsTrueWhenDone()
     {
+        $this->getQueue();
         $id = $this->_queue->dispatchTask($this->getTask());
-        if (!is_null($id)) {
-            do {
-                $ret = $this->_queue->checkTaskComplete($id);
-                sleep(1);
-            }
-            while (!$ret);
-            $this->assertTrue($ret);
+        do {
+            $ret = $this->_queue->checkTaskComplete($id);
+            sleep(1);
         }
+        while (!$ret);
+        $this->assertTrue($ret);
+        $this->getQueue('net');
+        $id = $this->_queue->dispatchTask($this->getTask());
+        $this->assertNull($id);
+        $this->assertNull(
+            $this->_queue->checkTaskComplete($id)
+        );
     }
 
     public function testCheckJobStatusReturnsTheRightValues()
     {
+        $this->getQueue();
         $id = $this->_queue->dispatchTask($this->getTask());
-        if (!is_null($id)) {
-            do {
-                $ret = $this->_queue->checkJobStatus($id);
-                if ($ret !== 'done' && $ret !== 'queued') {
-                    $this->assertGreaterThanOrEqual(0, $ret);
-                    $this->assertLessThanOrEqual(100, $ret);
-                }
-                sleep(1);
+        do {
+            $ret = $this->_queue->checkJobStatus($id);
+            if ($ret !== 'done' && $ret !== 'queued') {
+                $this->assertGreaterThanOrEqual(0, $ret);
+                $this->assertLessThanOrEqual(100, $ret);
             }
-            while ($ret !== 'done');
-            $this->assertTrue($ret === 'done');
+            sleep(1);
         }
+        while ($ret !== 'done');
+        $this->assertTrue($ret === 'done');
+        $this->getQueue('net');
+        $id = $this->_queue->dispatchTask($this->getTask());
+        $this->assertNull($id);
+        $this->assertNull(
+            $this->_queue->checkJobStatus($id)
+        );
     }
 
     public function testBlockingTaskReturnsResults()
     {
+        $this->getQueue();
         $ret = $this->_queue->blockingCall($this->getTask());
-        if (!class_exists('GearmanClient')) {
-            $this->assertNull($ret);
-        }
-        else{
-            $this->assertTrue($ret['payload'] == '!gnirts a si sihT');
-        }
+        $this->assertTrue($ret['payload'] == '!gnirts a si sihT');
+        $this->getQueue('net');
+        $ret = $this->_queue->blockingCall($this->getTask());
+        $this->assertNull($ret);
     }
 }
